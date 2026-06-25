dfd18c061c633d7d1de7544452535a5b916ffa30
Luis Fernando <lfpl_89@hotmail.com>
2026-06-25 16:09:17 -0300

    adicionando movimentos ao boneco

--- server.js
+++ server.js
@@ -1,33 +1,54 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-// Fala para o servidor entregar o arquivo index.html para quem acessar o site
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Quando alguém se conectar ao chat...
-io.on('connection', (socket) => {
-    console.log('Um usuário entrou no chat! ID: ' + socket.id);
+// Objeto para guardar as coordenadas de cada usuário conectado
+const usuarios = {};
 
-    // Quando o servidor receber uma mensagem de alguém...
-    socket.on('mensagem_enviada', (dados) => {
-        // Ele repassa essa mensagem para TODO MUNDO na sala
-        io.emit('mensagem_recebida', dados);
+io.on('connection', (socket) => {
+    console.log('Um usuário entrou! ID: ' + socket.id);
+
+    // Define uma posição inicial padrão para quem entra
+    usuarios[socket.id] = { x: 200, y: 200 };
+
+    // Envia a lista atualizada para todo mundo
+    io.emit('atualizar_usuarios', usuarios);
+
+    // Escuta quando um usuário se mexe
+    socket.on('mover', (dadosPosicao) => {
+        if (usuarios[socket.id]) {
+            usuarios[socket.id].x = dadosPosicao.x;
+            usuarios[socket.id].y = dadosPosicao.y;
+            
+            // Avisa todo mundo sobre a nova posição desse boneco
+            io.emit('atualizar_usuarios', usuarios);
+        }
     });
 
-    // Quando alguém fechar o site...
+    // Escuta quando alguém envia mensagem
+    socket.on('enviar_mensagem', (msg) => {
+        io.emit('usuario_falou', {
+            id: socket.id,
+            mensagem: msg
+        });
+    });
+
+    // Quando o usuário desconecta
     socket.on('disconnect', () => {
         console.log('Um usuário saiu do chat.');
+        delete usuarios[socket.id];
+        io.emit('atualizar_usuarios', usuarios);
     });
 });
 
-// O servidor vai rodar na porta 3000
-server.listen(3000, () => {
-    console.log('SERVIDOR RODANDO! Acesse: http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log('SERVIDOR RODANDO! Porta: ' + PORT);
 });
